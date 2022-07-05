3607afe86d1fad6b424e4e4abb7ab311029be7bb
Azat Azmetov <azmetoff@gmail.com>
2022-07-05 10:57:17 +0300

    add viewbinding

--- app/src/main/java/com/aztown/githubapi/MainActivity.kt
+++ app/src/main/java/com/aztown/githubapi/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.aztown.githubapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aztown.githubapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
